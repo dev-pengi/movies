31fa9313a82eac44e3a4e8a9d63724633c3d17b7
dev-pengi <siffffdoul@gmail.com>
2023-06-29 19:41:46 +0100

    Add portfolio link to the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
-import './Header.css'
+import { Link } from "react-router-dom";
+import "./Header.css";
 const Header = () => {
-    return (
-        <>
-            <header>
-                <div className="left">
-                    <h1>Look-it</h1>
-                </div>
-            </header>
-        </>
-    )
-}
+  return (
+    <>
+      <header>
+        <div className="left">
+          <h1>Look it</h1>
+        </div>
+        <div className="right">
+          <Link to={"https://sifedine.lol"}> more projects? </Link>
+        </div>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
